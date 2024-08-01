25a346bed9152125ad28ebfdccaf468c15c48eab
Danielyilma <yilmad752@gmail.com>
2024-08-01 20:38:21 +0300

    format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/use/bin/env python3
 '''logging by hiding secure data'''
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -8,3 +9,22 @@ def filter_datum(fields, redaction, message, separator):
         match = re.match(rf".*{field}=([^{separator}]+){separator}.*", message)
         message = re.sub(f"{match.group(1)}", redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        ''''''
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''formatting logs'''
+        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return logging.Formatter.format(self, record)
