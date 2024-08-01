ee085eed7ed7eea0172c43aeab1ed4e61401b0d5
Danielyilma <yilmad752@gmail.com>
2024-08-01 23:01:24 +0300

    1format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-""" Use of regex in replacing occurrences of certain field values """
+'''Filter logs with regex'''
 import re
 from typing import List
 import logging
-import mysql.connector
 import os
+import mysql.connector
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -20,71 +23,52 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Returns filtered values from log records """
+        '''Filter and format values'''
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
 
-PII_FIELDS = ("name", "email", "password", "ssn", "phone")
-
-
-def get_db() -> mysql.connector.connection.MYSQLConnection:
-    """ Connection to MySQL environment """
-    db_connect = mysql.connector.connect(
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
-    )
-    return db_connect
-
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ Returns regex obfuscated log messages """
-    for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''Filter definition using Regex'''
+    for f in fields:
+        message = re.sub(f + "=.*?" + separator,
+                         f + "=" + redaction + separator, message)
     return message
 
 
 def get_logger() -> logging.Logger:
-    """ Returns a logging.Logger object """
+    '''Definition that takes no arguments
+    and returns a logging.Logger object.'''
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
-    target_handler = logging.StreamHandler()
-    target_handler.setLevel(logging.INFO)
-
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handle.setFormatter(formatter)
-
-    logger.addHandler(target_handler)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(RedactingFormatter(PII_FIELDS))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
 
 
-def main() -> None:
-    """ Obtain database connection using get_db
-    retrieve all role in the users table and display
-    each row under a filtered format
-    """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-
-    headers = [field[0] for field in cursor.description]
-    logger = get_logger()
-
-    for row in cursor:
-        info_answer = ''
-        for f, p in zip(row, headers):
-            info_answer += f'{p}={(f)}; '
-        logger.info(info_answer)
-
-    cursor.close()
-    db.close()
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    '''Obtain te auth data from a env var'''
+    connector = mysql.connector.connect(
+        user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=os.environ.get("PERSONAL_DATA_DB_NAME"))
+    return connector
 
 
 if __name__ == '__main__':
-    main()
+    connection = get_db()
+    cursor = connection.cursor(dictionary=True)
+    query = ("SELECT * FROM users")
+    cursor.execute(query)
+    for row in cursor:
+        string = ""
+        for key in row:
+            string += "{}={}; ".format(key, row[key])
+        print(string)
+    cursor.close()
+    connection.close()
