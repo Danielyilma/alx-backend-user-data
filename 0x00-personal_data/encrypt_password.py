0a4f6346921a6eaf39f48bd280e243a64b666289
Danielyilma <yilmad752@gmail.com>
2024-08-02 12:13:08 +0300

    encription

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,12 +3,12 @@
 import bcrypt
 
 
-def hash_password(password: str) -> any:
+def hash_password(password: str) -> bytes:
     '''functio hash password using bcript hashpw'''
     salt = bcrypt.gensalt(rounds=12)
     return bcrypt.hashpw(password.encode(), salt)
 
 
-def is_valid(hashed_password: bytes, password: str) -> any:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     '''validate if a password match its hash'''
     return bcrypt.checkpw(password.encode(), hashed_password)
