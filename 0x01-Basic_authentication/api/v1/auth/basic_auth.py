461225a4b8431fe0e7183c909b1ffb35db6c9135
Danielyilma <yilmad752@gmail.com>
2024-08-06 22:15:42 +0300

    update extracting user credential

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,10 +49,11 @@ class BasicAuth(Auth):
 
         if ':' not in decoded_base64_authorization_header:
             return None, None
+        
+        idx = decoded_base64_authorization_header.find(':')
+        user_credential = decoded_base64_authorization_header
 
-        user_credential = decoded_base64_authorization_header.split(':')
-
-        return (user_credential[0], user_credential[1])
+        return (user_credential[:idx], user_credential[idx + 1:])
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
