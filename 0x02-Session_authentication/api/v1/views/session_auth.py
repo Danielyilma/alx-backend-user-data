fae44ed3eb0ab80c1c4b2270191033b985595ecc
Danielyilma <yilmad752@gmail.com>
2024-08-08 22:52:33 +0300

    session logout view

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -38,6 +38,7 @@ def login() -> str:
 
 @app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
+    '''logout from a session'''
     from api.v1.app import auth
     if not auth.destroy_session(request):
         abort(404)
