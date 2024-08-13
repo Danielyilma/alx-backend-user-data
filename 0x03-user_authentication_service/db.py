df3891762627831186127fccf556d8fb26256878
Danielyilma <yilmad752@gmail.com>
2024-08-13 11:56:16 +0300

    user model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import Base, User
 
@@ -40,9 +41,21 @@ class DB:
 
         return user
 
-    # def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs: dict) -> User:
+        '''find user by attribute'''
 
-    #     def check_attribute(attr):
-    #         if attr not in User.__dict__:
-    #             raise InvalidRequestError("Wrong attribute")
-    #         self._session.query(User, attr=)
+        def check_attribute(attr: str):
+            ''' checks the attribute is valid
+                if valid 
+                    return user found by that attribute
+            '''
+            if attr not in User.__dict__:
+                raise InvalidRequestError("Wrong attribute")
+
+            return self._session.query(User).filter(getattr(User, attr) == kwargs[attr]).first()
+        
+        result = list(map(check_attribute, kwargs))[0]
+
+        if not result:
+            raise NoResultFound("no result")
+        return result
