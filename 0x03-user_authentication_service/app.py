9735c72c3f9a71211e8e08f05c6212e46eddc101
Danielyilma <yilmad752@gmail.com>
2024-08-14 22:32:36 +0300

    session authentiaction service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,7 +46,7 @@ def login() -> str:
         abort(401)
 
     session_id = Auth.create_session(email)
-    response = jsonify({"email": "<user email>", "message": "logged in"})
+    response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
 
     return response
