654469b2ab7592d946ccb6733f0bfd570eebca29
Danielyilma <yilmad752@gmail.com>
2024-08-15 10:53:06 +0300

    adding reset_password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,6 +84,7 @@ def profile() -> str:
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
+    '''create reset_password token'''
     email = request.form.get('email')
 
     if not email:
@@ -99,6 +100,7 @@ def get_reset_password_token():
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
+    '''update user password'''
     email = request.form.get('email')
     reset_token = request.form.get('reset_password')
     new_password = request.form.get('new_password')
